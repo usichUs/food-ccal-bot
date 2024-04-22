5192f0cb012bca3b221cdc6cd0524bce18be6c2f
Nikita Usachev <guyfoks737@gmail.com>
2024-04-22 17:43:56 +0300

    started

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello")
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+import requests
+import time
+
+from settings import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def process_start_command(message: Message):
+    await message.answer("Hi!\nI'm EchoBot!\nPowered by aiogram and UsichUs!\nWrite me smth.")
+
+async def process_help_command(message: Message):
+    await message.answer("I don't have help :(")
+
+async def send_echo_photo(message: Message):
+    await message.answer_photo(message.photo[0].file_id)
+
+async def send_echo(message: Message):
+    await message.answer(text=message.text)
+
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=["help"]))
+dp.message.register(send_echo_photo, F.photo)
+dp.message.register(send_echo)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
---
+++ settings.py
@@ -0,0 +1,1 @@
+TOKEN = '5999427873:AAHhn3jug2DjtlQKycdDAw5kercVZ6N5po4'
