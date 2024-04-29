e9bf00f4d39cba9fcceb583013908bf1254ef761
Nikita Usachev <guyfoks737@gmail.com>
2024-04-29 15:32:29 +0300

    add buttons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 import json
 
 import requests
@@ -12,6 +12,11 @@ from functions.format_recipe_message import get_time_by_type
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+button_menu = KeyboardButton(text="Получить меню на день", callback_data="menu")
+button_help = KeyboardButton(text="Помощь", callback_data="help")
+
+keyboard = ReplyKeyboardMarkup(keyboard=[[button_menu, button_help]])
 
 async def process_menu_command(message: Message):
     # Читаем данные из файла JSON
@@ -34,26 +39,18 @@ async def process_menu_command_wrapper(message: Message):
     await process_menu_command(message)
 
 async def process_start_command(message: Message):
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text="Получить меню на день", callback_data="get_menu")
-    keyboard.add(button)
-    await message.answer("Нажмите кнопку, чтобы получить меню на день.", reply_markup=keyboard)
+    await message.answer("Привет! Я бот для получения рецептов. Напиши /help, чтобы узнать, что я умею.", reply_markup=keyboard)
 
 async def process_help_command(message: Message):
     await message.answer("I don't have help :(")
 
-async def send_echo(message: Message):
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(
-            text='Данный тип апдейтов не поддерживается '
-                 'методом send_copy')
-
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=["help"]))
 dp.message.register(process_menu_command_wrapper, Command(commands=["menu"]))
-dp.message.register(send_echo)
+dp.message.register(process_menu_command_wrapper, F.text == "Получить меню на день")
+dp.message.register(process_help_command, F.text == "Помощь")
+dp.callback_query.register(process_menu_command_wrapper, F.data == "menu")
+dp.callback_query.register(process_help_command, F.data == "help")
 
 if __name__ == '__main__':
     dp.run_polling(bot)
